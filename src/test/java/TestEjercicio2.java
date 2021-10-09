73ede40b6d9b95fde99275d0d233923a9b1c6c6a
ferraritech <91761962+ferraritech@users.noreply.github.com>
2021-10-08 22:23:04 -0500

    Create TestEjercicio2.java

---
+++ src/test/java/TestEjercicio2.java
@@ -0,0 +1,58 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+public class TestEjercicio2 extends Base{
+
+    @BeforeTest
+    public void initialize() {
+        driver = initializeDriver();
+        driver.get("https://demoqa.com/buttons");
+
+    }
+
+    @Test
+    public void VerifyClickBtn() {
+
+        Actions act = new Actions(driver);
+
+
+        WebElement DobleClick = driver.findElement(By.xpath("//*[@id=\"doubleClickBtn\"]"));
+        act.doubleClick(DobleClick).perform();
+
+        WebElement RightClick = driver.findElement(By.xpath("//*[@id=\"rightClickBtn\"]"));
+        act.contextClick(RightClick).perform();
+
+        WebElement OneClick = driver.findElement(By.xpath("/html/body/div[2]/div/div/div[2]/div[2]/div[1]/div[3]/button"));
+        act.click(OneClick).perform();
+
+        String DoubleClickText = driver.findElement(By.id("doubleClickMessage")).getText();
+        String RightClickText = driver.findElement(By.id("rightClickMessage")).getText();
+        String ClickMeText = driver.findElement(By.id("dynamicClickMessage")).getText();
+
+        String Text1 = "You have done a double click";
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertEquals(Text1, DoubleClickText);
+        softAssert.assertAll();
+
+        String Text2 = "You have done a right click";
+        SoftAssert softAssert2 = new SoftAssert();
+        softAssert2.assertEquals(Text2, RightClickText);
+        softAssert2.assertAll();
+
+        String Text3 = "You have done a dynamic click";
+        SoftAssert softAssert3 = new SoftAssert();
+        softAssert3.assertEquals(Text3, ClickMeText);
+        softAssert3.assertAll();
+    }
+
+    @AfterTest
+    public void closeDriver(){
+
+        driver.close();
+    }
+}
